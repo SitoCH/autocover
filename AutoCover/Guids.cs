af3b4b20d0043af84a1a7694bcac55ae0973ea20
Simone Grignola <sito@grignola.ch>
2013-07-05 08:08:02 +0200

    Switched from AddIn to Package

---
+++ AutoCover/Guids.cs
@@ -0,0 +1,14 @@
+﻿// Guids.cs
+// MUST match guids.h
+using System;
+
+namespace AutoCover
+{
+    static class GuidList
+    {
+        public const string guidAutoCoverPkgString = "90257b84-827f-4bd2-bfdc-b39a67c5d5b1";
+        public const string guidAutoCoverCmdSetString = "fc22dab0-e985-419b-a3c5-71e82d823f0f";
+
+        public static readonly Guid guidAutoCoverCmdSet = new Guid(guidAutoCoverCmdSetString);
+    };
+}
---
+++ AutoCover/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("AutoCover")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Simone Grignola")]
+[assembly: AssemblyProduct("AutoCover")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]   
+[assembly: ComVisible(false)]     
+[assembly: CLSCompliant(false)]
+[assembly: NeutralResourcesLanguage("en-US")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: InternalsVisibleTo("AutoCover_IntegrationTests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100f75ac3cf98a94aef4e3cc55814d09ec31d57f300c2126a072eacab14ba380c6cbfa08911051a1355307c7a295fc291b69fbe5c0ebfd24118d48b2cbfc65aa432f9878362eb386776e664dad1388493706c1e53a23f240b49231c7ce74acdd9919e2d1fe7408f1427a911a40a8aa543d3367f726dea7b42c554fc5e0bb13546b6")]
+[assembly: InternalsVisibleTo("AutoCover_UnitTests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100f75ac3cf98a94aef4e3cc55814d09ec31d57f300c2126a072eacab14ba380c6cbfa08911051a1355307c7a295fc291b69fbe5c0ebfd24118d48b2cbfc65aa432f9878362eb386776e664dad1388493706c1e53a23f240b49231c7ce74acdd9919e2d1fe7408f1427a911a40a8aa543d3367f726dea7b42c554fc5e0bb13546b6")]
